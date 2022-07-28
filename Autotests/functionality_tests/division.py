01c6186d0573e974cb07d80e3cfdcc6c8dfb4936
NelyaOs <nelya_osnova@gmail.com>
2022-07-28 11:25:45 +0700

    Formatted tests#2

--- Autotests/functionality_tests/division.py
+++ Autotests/functionality_tests/division.py
@@ -1,44 +1,54 @@
 from emuns import ResponseEndpoints, ResponseStatusCode
 from main import request_status_code, request_result
 
+
 def test_div_valid_values():
     assert request_result(ResponseEndpoints.division, 20, 10) == 2
+
 
 def test_div_min_value():
     assert request_result(ResponseEndpoints.division, -2147483648, 2) == -1073741824
 
+
 def test_div_max_values():
     assert request_result(ResponseEndpoints.division, 2147483647, 2147483647) == 1
+
 
 def test_div_min_values():
     assert request_result(ResponseEndpoints.division, -2147483648, -2147483648) == 1
 
+
 def test_div_min_plus_1():
     assert request_result(ResponseEndpoints.division, -2147483647, 1) == -2147483647
+
 
 def test_div_min_minus_1():
     assert request_status_code(ResponseEndpoints.division, -2147483649, 1) == ResponseStatusCode.large_value_error.value
 
+
 def test_div_max_minus_1():
     assert request_result(ResponseEndpoints.division, 2147483646, 1) == 2147483646
+
 
 def test_div_max_plus_1():
     assert request_status_code(ResponseEndpoints.division, 2147483648, 1) == ResponseStatusCode.large_value_error.value
 
+
 def test_div_zero_value():
     assert request_result(ResponseEndpoints.division, 0, 10) == 0
+
 
 def test_div_by_zero_value():
     assert request_status_code(ResponseEndpoints.division, 3467, 0) == ResponseStatusCode.calculate_error.value
 
+
 def test_div_fractional_number():
     assert request_status_code(ResponseEndpoints.division, 1.7, 10) == ResponseStatusCode.not_integer_error.value
+
 
 def test_div_empty_value():
     assert request_status_code(ResponseEndpoints.division, "", 111) == ResponseStatusCode.not_integer_error.value
 
+
 def test_div_str():
     assert request_status_code(ResponseEndpoints.division, "abc", 10) == ResponseStatusCode.not_integer_error.value
-
-
-
--- Autotests/functionality_tests/remainder.py
+++ Autotests/functionality_tests/remainder.py
@@ -5,43 +5,56 @@ from main import request_status_code, request_result
 def test_rem_valid_values():
     assert request_result(ResponseEndpoints.remainder, 20, 14) == 6
 
+
 def test_rem_max_and_min_values():
     assert request_result(ResponseEndpoints.remainder, -2147483648, 2147483647) == 2147483646
+
 
 def test_rem_max_values():
     assert request_result(ResponseEndpoints.remainder, 2147483647, 2147483647) == 0
 
+
 def test_rem_max_minus_1():
     assert request_result(ResponseEndpoints.remainder, 2147483646, 10) == 6
 
+
 def test_rem_max_plus_1():
-    assert request_status_code(ResponseEndpoints.remainder, 2147483648, 10) == ResponseStatusCode.large_value_error.value
+    assert request_status_code(ResponseEndpoints.remainder, 2147483648,
+                               10) == ResponseStatusCode.large_value_error.value
+
 
 def test_rem_min_values():
     assert request_result(ResponseEndpoints.remainder, -2147483648, -2147483648) == 0
 
+
 def test_rem_min_plus_1():
     assert request_result(ResponseEndpoints.remainder, -2147483647, 10) == 3
 
+
 def test_rem_min_minus_1():
-    assert request_status_code(ResponseEndpoints.remainder, -2147483649, 10) == ResponseStatusCode.large_value_error.value
+    assert request_status_code(ResponseEndpoints.remainder, -2147483649,
+                               10) == ResponseStatusCode.large_value_error.value
+
 
 def test_rem_by_zero_value():
     assert request_status_code(ResponseEndpoints.remainder, 15, 0) == ResponseStatusCode.calculate_error.value
 
+
 def test_rem_zero_value():
     assert request_result(ResponseEndpoints.remainder, 0, 15) == 0
+
 
 def test_rem_smaller_by_the_larger():
     assert request_result(ResponseEndpoints.remainder, 3, 15) == 3
 
+
 def test_rem_fractional_number():
     assert request_status_code(ResponseEndpoints.remainder, 1.7, 10) == ResponseStatusCode.not_integer_error.value
+
 
 def test_rem_empty_value():
     assert request_status_code(ResponseEndpoints.remainder, "", 111) == ResponseStatusCode.not_integer_error.value
 
+
 def test_rem_str():
     assert request_status_code(ResponseEndpoints.remainder, "abc", 10) == ResponseStatusCode.not_integer_error.value
-
-
